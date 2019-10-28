b2026a8aa00b9d092dffe106810a580900857344
BestNameYet <samuel.john.nixon@gmail.com>
2019-10-27 23:37:19 -0500

    Update model.js

--- model.js
+++ model.js
@@ -14,43 +14,47 @@ var myObject ={
 
 
 var TTTmodel = (
-  function(){
-    var model = {
-      board: [],
-      player1: {isComputer: false},
-      player2: {isComputer: false},
-      numMoves: 0,
-      isPlayer1Turn: false,
-      isPlayer2Turn: false,
-      isGamePaused: true,
-      hasWinner: false,
-      isPlayer1Winner: false,
-      isPlaeyr2Winner: false,
-      isTied: false,
-      get state(){
-        var stateObj = {
-          board: Array.from(this.board),
-          isPlayer1Computer: this.player1.isComputer,
-          isPlayer2Computer: this.player2.isComputer,
-          isPlayer1Turn: this.isPlayer1Turn,
-          isPlayer2Turn: this.isPlayer2Turn,
-          numMoves: this.numMoves,
-          hasWinner: this.hasWinner,
-          isPlayer1Winner: this.isPlayer1Winner,
-          isPlayer2Winner: this.isPlayer2Winner,
-          isTied: this.isTied
-        }
-        return stateObj;
-      },
-      move: function(number){
-        checkIfValid = function(){};
-      },
-      reset: function(){
-      }
-      
-            
-        
-      };//end of model declaration/initializtion
+	function(){
+		var board = [];
+		var player1 = {isComputer: false};
+		var player2 = {isComputer: false};
+		var numMoves = 0;
+		var isPlayer1Turn = false;
+		var isPlayer2Turn = false;
+		var isGamePaused = true;
+		var hasWinner = false;
+		var isPlayer1Winner = false;
+		var isPlayer2Winner = false;
+		var isTied = false;
+		var mainFunction = this;
+		var getState = function(){
+			var stateObj = {
+				board: Array.from(mainFunction.board),
+				isPlayer1Computer: mainFunction.player1.isComputer,
+				isPlayer2Computer: mainFunction.player2.isComputer,
+				isPlayer1Turn: mainFunction.isPlayer1Turn,
+				isPlayer2Turn: mainFunction.isPlayer2Turn,
+				numMoves: mainFunction.numMoves,
+				hasWinner: mainFunction.hasWinner,
+				isPlayer1Winner: mainFunction.isPlayer1Winner,
+				isPlayer2Winner: mainFunction.isPlayer2Winner,
+				isTied: mainFunction.isTied
+			};
+			return stateObj;
+		};
+		var tryMove({number: number}) = function(){};
+		var checkIfValid({number: number}) = function(){};
+		var checkForWinner({player1, player2, board}={}){};
+		var model = {
+			move: function(number){
+				checkIfValid = function(){};
+			},
+			reset: function(){}
+		};
+
+
+
+};//end of model declaration/initializtion
       return model};//end anonymous function
     
     
